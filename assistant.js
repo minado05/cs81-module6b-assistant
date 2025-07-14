38af67fb6268cb8f57e4ac9a72b64df35fdc83bc
minado05 <athudo@g.ucla.edu>
2025-07-13 21:37:58 -0700

    Tested assistant

--- assistant.js
+++ assistant.js
@@ -8,11 +8,36 @@ PersonalAssistant.prototype.addTask = function (task) {
   this.tasks.push(task);
 };
 
+PersonalAssistant.prototype.completeTask = function (task) {
+  this.tasks = this.tasks.filter((element) => element != task);
+  console.log("Completing task: ", task, "\n");
+};
+
+//if no argument, complete the first task in the list
 PersonalAssistant.prototype.completeTask = function () {
-  completedTask = this.tasks.shift();
-  console.log("Completing task: ", completedTask, nl);
+  let task = this.tasks.shift();
+  console.log("Completing task: ", task, "\n");
 };
 
 PersonalAssistant.prototype.reportMood = function () {
-  console.log("Current mood: ", this.mood, nl);
+  console.log("Current mood: ", this.mood, "\n");
 };
+
+PersonalAssistant.prototype.changeMood = function (mood) {
+  this.mood = mood;
+};
+
+let myAssistant = new PersonalAssistant("Mina");
+myAssistant.addTask("Do laundry");
+myAssistant.addTask("Do homework");
+myAssistant.addTask("Packing");
+myAssistant.addTask("Wash the dishes");
+myAssistant.changeMood("stressed");
+console.log("Hi! I'm ", myAssistant.name, "your assistant");
+console.log("You have ", myAssistant.tasks.length, " left");
+myAssistant.reportMood();
+myAssistant.completeTask();
+myAssistant.completeTask("Packing");
+myAssistant.changeMood("normal");
+console.log("Now you have ", myAssistant.tasks.length, " left");
+myAssistant.reportMood();
