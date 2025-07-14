455aa6ac11ebdb549919f7c57e018afdfa21ce63
minado05 <athudo@g.ucla.edu>
2025-07-13 21:23:43 -0700

    Added 3 methods

--- assistant.js
+++ assistant.js
@@ -3,3 +3,16 @@ function PersonalAssistant(name) {
   this.tasks = [];
   this.mood = null;
 }
+
+PersonalAssistant.prototype.addTask = function (task) {
+  this.tasks.push(task);
+};
+
+PersonalAssistant.prototype.completeTask = function () {
+  completedTask = this.tasks.shift();
+  console.log("Completing task: ", completedTask, nl);
+};
+
+PersonalAssistant.prototype.reportMood = function () {
+  console.log("Current mood: ", this.mood, nl);
+};
